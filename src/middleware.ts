1e98ebd7de0db7f032f71be823472ce480724f3a
koala819 <x.genolhac@gmail.com>
2024-02-04 18:52:14 +0100

    ready for 8pm

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,26 +1,23 @@
-import { NextRequest, NextResponse } from 'next/server'
+// import { NextRequest, NextResponse } from 'next/server'
 
-export function middleware(req: NextRequest) {
-  const url = req.nextUrl.clone()
-
-  const pathsToRedirect = [
-    '/accueil',
-    '/begin',
-    '/BonheurScore',
-    '/choix',
-    '/contact',
-    '/mentions',
-    '/promo',
-  ]
-
-  const shouldRedirect = pathsToRedirect.some((path) =>
-    url.pathname.startsWith(path),
-  )
-
-  if (shouldRedirect) {
-    url.pathname = '/'
-    return NextResponse.redirect(url)
-  }
-
-  return NextResponse.next()
+export function middleware() {
+  // export function middleware(req: NextRequest) {
+  // const url = req.nextUrl.clone()
+  // const pathsToRedirect = [
+  //   '/accueil',
+  //   '/begin',
+  //   '/BonheurScore',
+  //   '/choix',
+  //   '/contact',
+  //   '/mentions',
+  //   '/promo',
+  // ]
+  // const shouldRedirect = pathsToRedirect.some((path) =>
+  //   url.pathname.startsWith(path),
+  // )
+  // if (shouldRedirect) {
+  //   url.pathname = '/'
+  //   return NextResponse.redirect(url)
+  // }
+  // return NextResponse.next()
 }
