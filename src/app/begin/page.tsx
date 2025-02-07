24a5f1d97482bf08d9bd0986b85b2d97e42bf428
Fabien.Wheel <fabien.wheeler@gmail.com>
2025-02-07 13:59:08 +0100

    page.tsx
    Texte encapsulé

--- src/app/begin/page.tsx
+++ src/app/begin/page.tsx
@@ -34,17 +34,20 @@ const Page = () => {
         </h3>
       </div>
 
+      
       <p>
+        <h3>
         Si tu veux te mettre à la roue électrique mais que tu as peur de te
-        lancer, je propose des sessions de formation théorique et pratique pour
+        lancer, je propose des <strong>sessions de formation</strong> théorique et pratique pour
         t’accompagner et dédramatiser. En une heure, tu auras un autre regard
-        sur l’objet. Pour prendre rendez-vous:{' '}
+        sur l’objet. <strong>Pour prendre rendez-vous:{' '}
         <Link
           href={'mailto:bonheursurseine@gmail.com'}
           className="hover:underline"
         >
           bonheursurseine@gmail.com
-        </Link>
+        </Link></strong>
+          </h3>
       </p>
 
       <CommonSense />
