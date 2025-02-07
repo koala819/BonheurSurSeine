8de5825bebc3877cbab92180182bb7d726e02032
Fabien.Wheel <fabien.wheeler@gmail.com>
2025-02-07 17:12:44 +0100

    Navbar.tsx

--- src/components/templates/Navbar.tsx
+++ src/components/templates/Navbar.tsx
@@ -98,11 +98,11 @@ export function Top() {
           </Link>
         </NavbarItem>
 
-        {/* <NavbarItem isActive={path.includes('/contact')}>
+        <NavbarItem isActive={path.includes('/contact')}>
           <Link href="/contact" className="text-white hover:text-text-link">
             Contact
           </Link>
-        </NavbarItem> */}
+        </NavbarItem>
       </NavbarContent>
       <NavbarContent justify="end">
         <NavbarItem>
