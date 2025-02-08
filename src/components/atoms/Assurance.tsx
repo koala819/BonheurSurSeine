50dd2852f6c4290ef255d0aa22e7566437a342e6
Fabien.Wheel <fabien.wheeler@gmail.com>
2025-02-08 18:02:08 +0100

    docs : précisions et ajout de liens
    Prise en compte des évolutions sur la vignette verte

--- src/components/atoms/Assurance.tsx
+++ src/components/atoms/Assurance.tsx
@@ -17,53 +17,50 @@ const Assurance = () => {
           <div className="blueBlock mb-8">
             <h3>
               🛡️ L&apos;assurance est{' '}
-              <span className="text-xl sm:text-xl md:text-2xl underline">
+              <span className="text-xl sm:text-xl md:text-2xl underline text-brown-500">
                 obligatoire
               </span>{' '}
-              !
-              <Link
-                href="https://www.legifrance.gouv.fr/codes/article_lc/LEGIARTI000025043014"
-                target="_blank"
-                className="link-style"
-              >
-                (L324-1)
-              </Link>
+              ! (
+              <Link href="https://www.legifrance.gouv.fr/codes/article_lc/LEGIARTI000025043014"
+                target="_blank" className="link-style">
+                L324-1
+              </Link>)
             </h3>
             <span>
-              Si quelqu&apos;un te dit autre chose, soit il ment, soit il est
-              incompétent.
+              Si quelqu&apos;un te dit autre chose, soit il ment, soit il est incompétent.
             </span>
           </div>
           <h3>À retenir :</h3>
           <ul className="list-disc pl-4 space-y-4 mb-4">
-            <li>
-              La roue est un <strong>véhicule terrestre à moteur</strong> et,
-              elle doit être assurée. Le type de contrat est au minimum en{' '}
-              <strong className="text-brown-500">responsabilité civile</strong>.
-            </li>
-            <li>La vignette (papillon vert) doit être affichée.</li>
-            <li>
-              Attention, les contrats d’assurance habitation ne couvrent
-              généralement pas les EDPM, catégorie à laquelle appartiennent les
-              roues. Il faut un contrat d’assurance dédié (comme pour une moto
-              ou une voiture).
-            </li>
-            <li>
-              En l&apos;absence d’assurance, tu risques une amende (jusqu’à
-              3750€ -
-              <Link
-                href="https://www.legifrance.gouv.fr/codes/article_lc/LEGIARTI000025043014"
-                target="_blank"
-                className="link-style"
-              >
-                L324-2
-              </Link>
-              ), la confiscation du véhicule, et surtout des dizaines de
-              milliers d’euros à charge en cas de collision/blessure d’un autre
-              usager.
-            </li>
+            <li>La roue est un <strong>véhicule terrestre à moteur</strong> et,
+				elle doit être assurée. Le type de contrat est au minimum en{' '}
+				<strong className="text-brown-500">responsabilité civile</strong>.
+				</li>
+            <li>La vignette (papillon vert) doit être affichée sur le véhicule (
+				<Link href="https://www.service-public.fr/particuliers/actualites/A17230"
+				target="_blank" className="link-style">à la différence des véhicules immatriculées</Link>
+				{' '}, voir les articles{' '} 
+				<Link href="https://www.legifrance.gouv.fr/codes/article_lc/LEGIARTI000006812169"
+				target="_blank" className="link-style">R211-21-1</Link>
+				{' '}et{' '}
+				<Link href="https://www.legifrance.gouv.fr/codes/article_lc/LEGIARTI000006812176"
+				target="_blank" className="link-style">R211-21-5</Link>
+				), en plus d’avoir sur soi l&apos;attestation d&apos;assurance (carte verte -{' '} 
+				<Link href="https://www.legifrance.gouv.fr/codes/article_lc/LEGIARTI000048547657"
+				target="_blank" className="link-style">R211-21-14</Link>).
+				</li>
+            <li>Attention, les contrats d&apos;assurance habitation ne couvrent
+				généralement pas les EDPM, catégorie à laquelle appartiennent les
+				roues. Il faut un contrat d&apos;assurance dédié (comme pour une moto ou une voiture).
+				</li>
+            <li>En l&apos;absence d&apos;assurance, tu risques une amende (jusqu&apos;à 3750€ -{' '}
+				<Link href="https://www.legifrance.gouv.fr/codes/article_lc/LEGIARTI000025043014"
+                target="_blank" className="link-style">L324-2</Link>
+				), la confiscation du véhicule, et surtout des dizaines de
+				milliers d&apos;euros à charge en cas de collision/blessure d&apos;un autre usager.
+				</li>
           </ul>
-          <div className="blueBlock mb-8">
+          <div className="yellowBlock mb-8">
             <h3>
               📢 “S&apos;assurer, ce n&apos;est pas une question de sécurité,
               c&apos;est une question de responsabilité !”
@@ -71,7 +68,7 @@ const Assurance = () => {
           </div>
           <p>
             Assurer un EDPM coûte 5-10€/mois. La quasi totalité des assureurs
-            propose des contrats d’assurance adaptés. Tu trouveras un{' '}
+            propose des contrats d&apos;assurance adaptés. Tu trouveras un{' '}
             <Link href="/promo" className="link-style">
               code Promo
             </Link>{' '}
@@ -79,7 +76,7 @@ const Assurance = () => {
             partenaire de la chaine.
           </p>
           <p className="text-gray-500 dark:text-gray-300 mt-4 text-xs">
-            article rédigé en janvier 2024
+            article mis à jour en janvier 2025
           </p>
         </AccordionItem>
       </Accordion>
