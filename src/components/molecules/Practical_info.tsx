30c8b88747b66e58b247364e5064197cf9f7bdbf
Fabien.Wheel <fabien.wheeler@gmail.com>
2025-02-20 19:32:39 +0100

    fix: neutralise partie dico
    neutralisation partie dico dans la page SAVOIR UTILE

--- src/components/molecules/Practical_info.tsx
+++ src/components/molecules/Practical_info.tsx
@@ -1,10 +1,9 @@
-import Dico from '@/src/components/atoms/Practical_Dico_old'
+//import Dico from '@/src/components/atoms/Practical_Dico'
 import Video from '@/src/components/atoms/Practical_Video'
 
 const Practical_info = () => {
   return (
     <div className="py-10 px-4 sm:px-6 lg:px-8 text-lg container mx-auto p-4">
-      <Dico />
       <Video />
     </div>
   )
