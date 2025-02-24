9a18b440b767d855d42337f2a7e2366e1619d624
koala819 <x.genolhac@gmail.com>
2025-02-24 13:56:00 +0100

    fix: increase result from prismic in BonheurScore

--- src/app/BonheurScore/page.tsx
+++ src/app/BonheurScore/page.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from 'next'
 import { notFound } from 'next/navigation'
 
+import { BonheurScoreProps } from '@/src/types/models'
+
 import { BonheurScore } from '@/src/components/atoms/BonheurScore'
 
 import { createClient } from '@/prismicio'
@@ -17,13 +19,28 @@ export const metadata: Metadata = {
 export default async function Page() {
   const client = createClient()
   try {
-    const gyroroues = await client.getByType('gyroroue', {
-      orderings: {
-        field: 'my.gyroroue.date',
-        direction: 'desc',
-      },
-    })
-    return <BonheurScore gyroroues={gyroroues.results} />
+    let allGyroroues: BonheurScoreProps[] = []
+    let page = 1
+    let hasMorePages = true
+
+    while (hasMorePages) {
+      const gyroroues = await client.getByType('gyroroue', {
+        orderings: {
+          field: 'my.gyroroue.date',
+          direction: 'desc',
+        },
+        page,
+        pageSize: 100, // Maximum autorisé par Prismic
+      })
+
+      allGyroroues = [...allGyroroues, ...gyroroues.results]
+
+      // Vérifier s'il y a plus de pages
+      hasMorePages = gyroroues.next_page !== null
+      page++
+    }
+
+    return <BonheurScore gyroroues={allGyroroues} />
   } catch (error) {
     notFound()
   }
