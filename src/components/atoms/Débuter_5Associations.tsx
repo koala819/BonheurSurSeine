96fd53d7117a00a5d307ecff57ad6975e28851e7
Fabien.Wheel <fabien.wheeler@gmail.com>
2025-09-26 14:26:30 +0200

    feat: REFONTEfix19

--- src/components/atoms/Débuter_5Associations.tsx
+++ src/components/atoms/Débuter_5Associations.tsx
@@ -9,12 +9,15 @@ import Link from 'next/link'
 
 import Discord from '@/public/Discord.png'
 import Discord2 from '@/public/Discord_screenshot.jpg'
-import Anumme from '@/public/marques/Anumme.png'
+import Anumme from '@/public/marques/Anumme.jpg'
 
 const Associations = () => {
   //CODE POUR OUVRIR LA SECTION AUTOMATIQUEMENT
   const [openKeys, setOpenKeys] = useState<string[]>([])
   const [scrollTarget, setScrollTarget] = useState<string | null>(null)
+
+  const [isOpen, setIsOpen] = useState(false)
+
   useEffect(() => {
     const handleHashChange = () => {
       const hash = window.location.hash.replace('#', '')
@@ -57,49 +60,19 @@ const Associations = () => {
           title={<h3>🌍 Groupes & associations</h3>}
           indicator={<strong className="chevronAccordionItem">&lsaquo;</strong>}
         >
-          <section>
-            <h3>
-              L&apos;<span className="small-caps">ANUMME</span>
-            </h3>
-            <div className="blueBlock mb-2">
-              <h4>
-                ℹ️ Leur site est très complet et bourré d&apos;informations
-              </h4>
-            </div>
-            <div className="flex flex-col lg:flex-row">
-              <aside className="lg:w-7/12 space-y-8">
-                <p className="text-justify">
-                  L&apos;
-                  <strong>
-                    Association Nationale des Utilisateurs de Micro-Mobilité
-                    Électrique
-                  </strong>{' '}
-                  a été créée par des passionnés afin de promouvoir et défendre
-                  l&apos;utilisation des EDPM (roues, trottinettes, e-skate).
-                  <br /> <br />
-                  Même si l&apos;asso est Leur site est rempli
-                  d&apos;informations utiles.
-                </p>
-              </aside>
+          {/* Le Bonheur */}
+          <div className="blueBlock mt-0 mb-0">
+            <h4 className="mt-0 mb-0">
+              📢 &laquo;&nbsp;Le bonheur n&apos;est réel que lorsqu&apos;il est
+              partagé.&nbsp;&raquo;
+            </h4>
+          </div>
 
-              <aside className="lg:w-5/12 flex items-center justify-center">
-                <Link href={'https://www.anumme.fr/'} passHref target="_blank">
-                  <Image
-                    src={Anumme}
-                    alt="Anumme Logo"
-                    width={300}
-                    height={500}
-                    className="rounded-lg cursor-pointer"
-                  />
-                </Link>
-              </aside>
-            </div>
-          </section>
-
+          {/* Invitation Discord */}
           <section className="mt-6">
-            <h3>Le salon Discord</h3>
-            {/* Invitation Discord */}
-            <div className="mt-4 text-center ">
+            <h4 className="font-semibold">🟢 Le salon Discord</h4>
+
+            <div className="ml-2 mt-4 text-center">
               <Link
                 href="https://discord.com/invite/Jhgw7C96Jf"
                 target="_blank"
@@ -116,7 +89,7 @@ const Associations = () => {
                 Discord.
               </Link>
             </div>
-            <div className="mt-2 max-w-6xl mx-auto grid md:grid-cols-[1fr_minmax(420px,1fr)] gap-2 items-center">
+            <div className="ml-2 mt-2 max-w-6xl mx-auto grid md:grid-cols-[1fr_minmax(420px,1fr)] gap-2 items-center">
               {/* Colonne de gauche */}
               <div className="mb-0">
                 <div className="items-center">
@@ -155,104 +128,169 @@ const Associations = () => {
               </div>
 
               {/* Colonne de droite */}
-              <div className="relative max-w-4xl mx-auto order-first md:order-last ">
-                <Link href={Discord2.src} passHref target="_blank">
-                  <Image
-                    src={Discord2}
-                    alt="Salon Discord de la chaîne"
-                    width={900}
-                    height={800}
-                    className="w-full max-w-lg mx-auto rounded-2xl object-cover object-center shadow-lg"
-                    priority
-                    placeholder="blur"
-                  />
-                </Link>
+              <div className="relative max-w-4xl mx-auto order-first md:order-last">
+                <Image
+                  src={Discord2}
+                  alt="Salon Discord de la chaîne"
+                  width={900}
+                  height={800}
+                  className="w-full max-w-lg mx-auto rounded-2xl object-cover object-center cursor-pointer shadow-md hover:shadow-lg dark:shadow-slate-600"
+                  priority
+                  placeholder="blur"
+                  onClick={() => setIsOpen(true)} // 👈 ouvre la lightbox
+                />
               </div>
+
+              {/* Lightbox */}
+              {isOpen && (
+                <div
+                  className="fixed inset-0 bg-black bg-opacity-70 flex items-center justify-center z-50"
+                  onClick={() => setIsOpen(false)} // clic fond ferme
+                >
+                  <div className="relative max-w-5xl w-full p-4 ">
+                    <button
+                      className="absolute top-4 right-4 text-blue-300 text-5xl font-bold"
+                      onClick={() => setIsOpen(false)}
+                    >
+                      ✕&nbsp;
+                    </button>
+                    <Image
+                      src={Discord2}
+                      alt="Salon Discord de la chaîne"
+                      width={800}
+                      height={800}
+                      className="w-full h-auto rounded-xl shadow-lg border-medium border-blue-300"
+                      priority
+                    />
+                  </div>
+                </div>
+              )}
             </div>
 
             <div></div>
           </section>
 
+          {/* ANUMME */}
           <section className="mt-6">
-            <h3>Les associations</h3>
-            <div className="blueBlock mb-2">
-              <h4>
-                📢 &laquo;&nbsp;Le bonheur n&apos;est réel que lorsqu&apos;il
-                est partagé.&nbsp;&raquo;
+            <h4 className="font-semibold">
+              🟢 L&apos;<span className="small-caps">ANUMME</span>
+            </h4>
+            <div className="blueBlock mb-4">
+              <h4 className="mt-0 mb-0">
+                ℹ️ Leur site est très complet et bourré d&apos;informations
               </h4>
             </div>
-            {/* Carte des asso */}
-            <div>
-              <p className="mt-0 text-justify">
-                Si tu te sens seul dans ta région, ce n&apos;est surement
-                qu&apos;une fausse impression&nbsp;!
-                <br />
-                Des groupes et des associations existent un peu partout. Tu en
-                trouveras plein sur cette carte (magnifique travail réalisé par{' '}
-                <Link
-                  //href={'https://leswheelerssarthois.fr/'}
-                  href={'https://leswheelerssarthois.asptt.com/'}
-                  passHref
-                  target="_blank"
-                  className="link-style"
-                >
-                  Les Wheelers Sarthois
-                </Link>{' '}
-                que je remercie) :
-              </p>
-              <div className="flex flex-col lg:flex-row mt-2 mb-2">
-                <aside className="lg:w-7/12 flex items-center justify-center ">
-                  <iframe
-                    src="https://www.google.com/maps/d/embed?mid=1Y9MEriN0xb-_4wEnTdJdRho7BXps4cy1&ehbc=2E312F"
-                    width="640"
-                    height="480"
-                    className="rounded-md"
-                  ></iframe>
-                </aside>
+            <div className="max-w-6xl mx-auto gap-x-4 px-6 flex flex-col lg:flex-row">
+              <aside className="lg:w-7/12 space-y-2">
+                <p className="text-justify">
+                  L&apos;
+                  <strong>
+                    Association Nationale des Utilisateurs de Micro-Mobilité
+                    Électrique
+                  </strong>{' '}
+                  a été créée par des passionnés afin de promouvoir et défendre
+                  l&apos;utilisation des EDPM (roues, trottinettes, e-skate).
+                </p>
+                <p>Leur site est rempli d&apos;informations utiles.</p>
+              </aside>
 
-                <aside className="lg:w-5/12 pt-8 lg:pt-0 lg:pl-8">
-                  <p>Autant d&apos;opportunités de : </p>
-                  <ul className="compactlist2 mt-1 mb-4">
-                    <li>
-                      <strong className="text-blue-800 dark:text-blue-400">
-                        Rencontrer
-                      </strong>{' '}
-                      d&apos;autres passionnés.
-                    </li>
-                    <li>
-                      <strong className="text-blue-800 dark:text-blue-400">
-                        Partager
-                      </strong>{' '}
-                      des expériences autour de cette passion commune.
-                    </li>
-                    <li>
-                      <strong className="text-blue-800 dark:text-blue-400">
-                        Découvrir
-                      </strong>{' '}
-                      d&apos;autres façons de faire de la roue (freestyle,
-                      randonneurs, racer, off road, acrobates, etc.).
-                    </li>
-                    <li>
-                      <strong className="text-blue-800 dark:text-blue-400">
-                        Participer
-                      </strong>{' '}
-                      à l&apos;organisation d&apos;événements.
-                    </li>
-                    <li>Faire connaitre la roue électrique.</li>
-                  </ul>
-                  <div className="relative w-full pb-[56.25%]">
-                    <iframe
-                      className="absolute top-0 left-0 w-full h-full rounded-xl"
-                      src="https://www.youtube.com/embed/iQzrhgo2XiM?si=k2ZEL-ZZFjGE6YJq"
-                      title="La communauté de la roue"
-                      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                    ></iframe>
-                  </div>
-                  <p className="mt-0 mb-0 text-xs place-items-center items-center text-center italic text-gray-600 dark:text-gray-400">
-                    Vidéo : La communauté de la roue
-                  </p>
-                </aside>
+              <aside className="lg:w-5/12 flex items-center justify-center">
+                <Link href={'https://www.anumme.fr/'} passHref target="_blank">
+                  <Image
+                    src={Anumme}
+                    alt="Anumme Logo"
+                    width={300}
+                    height={500}
+                    className="rounded-lg cursor-pointer shadow-md hover:shadow-lg dark:shadow-slate-600"
+                  />
+                </Link>
+              </aside>
+            </div>
+          </section>
+
+          {/*--------------------------------------------*/}
+          {/*              Secion asso                   */}
+          {/*--------------------------------------------*/}
+          <section className="mt-6">
+            <h4 className="font-semibold">🟢 Les associations</h4>
+            {/* ------------------ Comment ça marche ------------------ */}
+            <div className="max-w-6xl mx-auto grid grid-cols-1 lg:grid-cols-2 gap-x-4 items-center px-6">
+              <div className="place-content-center mb-4">
+                <p className="mt-0 mb-0">
+                  Si tu te sens seul dans ta région, ce n&apos;est surement
+                  qu&apos;une fausse impression&nbsp;!
+                  <br />
+                  Ce sont autant d&apos;opportunités de :{' '}
+                </p>
+                <ul className="compactlist2 mt-0 mb-2">
+                  <li>
+                    <strong className="text-blue-800 dark:text-blue-400">
+                      Rencontrer
+                    </strong>{' '}
+                    d&apos;autres passionnés.
+                  </li>
+                  <li>
+                    <strong className="text-blue-800 dark:text-blue-400">
+                      Partager
+                    </strong>{' '}
+                    des expériences autour de cette passion commune.
+                  </li>
+                  <li>
+                    <strong className="text-blue-800 dark:text-blue-400">
+                      Découvrir
+                    </strong>{' '}
+                    d&apos;autres façons de faire de la roue (freestyle,
+                    randonneurs, racer, off road, acrobates, etc.).
+                  </li>
+                  <li>
+                    <strong className="text-blue-800 dark:text-blue-400">
+                      Participer
+                    </strong>{' '}
+                    à l&apos;organisation d&apos;événements.
+                  </li>
+                  <li>Faire connaitre la roue électrique.</li>
+                </ul>
+                <p className="mt-0 mb-0">
+                  Des groupes et des associations existent un peu partout.
+                </p>
+                <p className="mt-0 mb-2">
+                  Tu en trouveras plein sur cette carte (magnifique travail
+                  réalisé par{' '}
+                  <Link
+                    //href={'https://leswheelerssarthois.fr/'}
+                    href={'https://leswheelerssarthois.asptt.com/'}
+                    passHref
+                    target="_blank"
+                    className="link-style"
+                  >
+                    Les Wheelers Sarthois
+                  </Link>{' '}
+                  que je remercie).
+                </p>
               </div>
+              {/* ------------------ Vidéo ------------------ */}
+              <div className="place-content-center place-items-center order-first lg:order-last">
+                <div className="w-full sm:w-4/5 md:w-3/4 lg:w-full aspect-video rounded-xl shadow-md overflow-hidden">
+                  <iframe
+                    className="w-full h-full"
+                    src="https://www.youtube.com/embed/iQzrhgo2XiM?si=k2ZEL-ZZFjGE6YJq"
+                    title="La communauté de la roue"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                  ></iframe>
+                </div>
+                <p className="text-sm italic text-gray-600 dark:text-gray-400 text-center">
+                  La communauté de la roue
+                </p>
+              </div>
+            </div>
+            {/* ------------------ GOOGLE MAPS ------------------ */}
+            <div className="max-w-6xl mx-auto gap-x-4 place-items-center px-6">
+              <iframe
+                src="https://www.google.com/maps/d/embed?mid=1Y9MEriN0xb-_4wEnTdJdRho7BXps4cy1&ehbc=2E312F"
+                width="640"
+                height="480"
+                className="rounded-md"
+              ></iframe>
             </div>
           </section>
 
