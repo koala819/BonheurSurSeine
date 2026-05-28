b56aa12a06f63f760a807011df6537cccf21d55a
Fabien.Wheel <fabien.wheeler@gmail.com>
2026-05-28 13:38:49 +0200

    feat: annulation
    bloc MEW normal

--- src/app/codes-promo/page.tsx
+++ src/app/codes-promo/page.tsx
@@ -47,10 +47,11 @@ export default async function Page() {
       </p>
 
       {/* Capsule Boutique avec Timer */}
-      {/* <BoutiqueMEWfr /> */}
+      {/* -------
       <div className="md:mx-4">
         <BoutiqueMEWfr />
       </div>
+      */}
 
       {/* Grille de promos partenaires */}
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-2 md:gap-3 lg:gap-5">
