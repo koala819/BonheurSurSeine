875fa85cfc1ead7b9857789471ce1dce692d03ae
Fabien.Wheel <fabien.wheeler@gmail.com>
2026-06-28 14:19:38 +0200

    fix: titre et accès à image

--- src/app/guide-utile-gyroroue/page.tsx
+++ src/app/guide-utile-gyroroue/page.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from 'next'
-//import Info from '@/src/components/atoms/SavoirUtile_6Infos'
 import Image from 'next/image'
+import Link from 'next/link'
 
 import Dico from '@/src/components/atoms/SavoirUtile_1Dico'
 import Maintenance from '@/src/components/atoms/SavoirUtile_2Entretien'
@@ -10,13 +10,14 @@ import Suspension from '@/src/components/atoms/SavoirUtile_3Suspension'
 import Navi from '@/src/components/atoms/SavoirUtile_4Navi'
 import Vrac from '@/src/components/atoms/SavoirUtile_5Vrac'
 
+//import Info from '@/src/components/atoms/SavoirUtile_6Infos'
 import EUC_idées_clés from '@/public/4.guide/BSS - 3 idées clés sur la roue.png'
 
 export const metadata: Metadata = {
   title:
     'Tout Savoir sur la gyroroue : Fonctionnement, Guide et Infos Pratiques sur les gyroroues / monoroues / roues électriques',
   description:
-    'Fabricants, entretien, batteries, pneus, conseils, guide pratique pour aller plus loin, trucs et astuce pour mieux comprendre la roue électrique. Devenez expert et roulez en toute confiance !',
+    'Entretien, batterie, pneus, suspension, conseils, guide pratique pour aller plus loin, trucs et astuce pour mieux comprendre la roue électrique. Devenez expert et roulez en toute confiance !',
   alternates: {
     canonical: `${process.env.CLIENT_URL}/guide-utile-gyroroue`,
   },
@@ -26,40 +27,53 @@ const Page = () => {
   return (
     <div className="py-10 px-2 sm:px-4 md:px-6 lg:px-8 space-y-8">
       <h1>Guide pratique</h1>
-      <div className="space-y-1">
-        <div className="mt-0 mb-0 flex flex-col md:flex-row items-center gap-x-2 gap-y-0">
-          <aside className="md:w-1/2 space-y-1">
-            <p className="text-left mt-0 mb-0">
-              Ce <strong>guide complet</strong> sur la gyroroue contient des{' '}
-              <strong>infos pratiques</strong> et des conseils{' '}
-              <strong>pour bien utiliser ta roue</strong>&nbsp;: fonctionnement,
-              entretien, autonomie, batterie, sécurité, applications pratiques
-              et confort de conduite...
-            </p>
-          </aside>
-          <aside className="md:w-1/2 blueBlock mt-0 mb-0">
-            <h4 className="mt-0 mb-0">
-              🎓 Qui a dit &laquo;&nbsp;un wheeler averti en vaut
-              deux&nbsp;&raquo;&nbsp;?
-            </h4>
-          </aside>
+      <div>
+        <div className="space-y-1">
+          <div className="mt-0 mb-0 flex flex-col md:flex-row items-center gap-x-2 gap-y-0">
+            <aside className="md:w-1/2 space-y-1">
+              <p className="text-left mt-0 mb-0">
+                Ce <strong>guide complet</strong> sur la gyroroue contient des{' '}
+                <strong>infos pratiques</strong> et des conseils{' '}
+                <strong>pour bien utiliser ta roue</strong>&nbsp;:
+                fonctionnement, entretien, autonomie, batterie, pneus, sécurité,
+                suspension, applications pratiques et confort de conduite...
+              </p>
+            </aside>
+            <aside className="md:w-1/2 blueBlock mt-0 mb-0">
+              <h4 className="mt-0 mb-0">
+                🎓 Qui a dit &laquo;&nbsp;un wheeler averti en vaut
+                deux&nbsp;&raquo;&nbsp;?
+              </h4>
+            </aside>
+          </div>
         </div>
+        <div
+          className="mt-4 justify-center
+        bg-white dark:bg-gray-700
+          rounded-lg
+          border border-gray-200 dark:border-gray-600
+          shadow-md hover:shadow-lg transition-shadow duration-300
+        shadow-gray-400 hover:shadow-gray-400
+        dark:shadow-neutral-900 dark:hover:shadow-neutral-950"
+        >
+          <Link href={EUC_idées_clés.src} passHref target="_blank">
+            <Image
+              src={EUC_idées_clés}
+              alt="3 idées clés sur la gyroroue"
+              className="rounded-lg cursor-pointer shadow-md transition-shadow"
+            />
+          </Link>
+        </div>
+        <p
+          className="text-xs md:text-sm italic text-gray-600 dark:text-gray-400 text-center
+        mt-2"
+        >
+          Infographie : Comprendre facilement la gyroroue <br />
+          Les 3 chaînes de perception, de commande et de puissance sont limitées
+          par le maillon le plus faible.
+        </p>
       </div>
-      <div
-        className="mt-0 mb-6 flex justify-center
-       rounded-lg
-      border border-gray-200 dark:border-gray-600
-      shadow-md hover:shadow-lg transition-shadow duration-300
-     shadow-gray-400 hover:shadow-gray-400
-     dark:shadow-neutral-900 dark:hover:shadow-neutral-950
-         "
-      >
-        <Image
-          src={EUC_idées_clés}
-          alt="3 idées clés sur la gyroroue"
-          className="rounded-lg"
-        />
-      </div>
+
       <Dico />
       <Maintenance />
       <Battery />
