107caedfa8c4bb1c6c9038754191e15b4a7825f5
Fabien.Wheel <fabien.wheeler@gmail.com>
2026-07-22 13:41:43 +0200

    feat: test

--- src/components/atoms/Choisir_3Needs.tsx
+++ src/components/atoms/Choisir_3Needs.tsx
@@ -5,7 +5,7 @@ import { useState } from 'react'
 import Image from 'next/image'
 import Link from 'next/link'
 
-import { QuizBesoins_modal } from '@/src/components/atoms/Choisir_3Needs-quizz modal'
+//import { QuizBesoins_modal } from '@/src/components/atoms/Choisir_3Needs-quizz modal'
 import { QuizBesoins_beta } from '@/src/components/atoms/Choisir_3Needs-quizz vbeta'
 
 import Image_bon_sens from '@/public/2.choisir/Image_choisir_identifier_besoins(light)2.jpg'
@@ -272,7 +272,7 @@ const Needs = () => {
       </div>
 
       {/*-- APPEL DU COMPOSANT INTERACTIF --*/}
-      <QuizBesoins_modal />
+
       <QuizBesoins_beta />
       {/*----------------------------------------------*/}
       {/*--                  BLOCS                   --*/}
